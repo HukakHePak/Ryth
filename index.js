4a35c3e7e719c86253867af7a38c9e153170c083
Hukak_He_Pak <valsidalvonilb@outlook.com>
2022-01-08 14:36:26 +0400

    add: command crumber, find tracks on yt

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const { Client, Intents, VoiceChannel} = require('discord.js');
 const { token } = require('./config.json');
+//const { playlists } = require('./playlists.json');
 //const { createReadStream, createWriteStream } = require('fs');
 //const play = require('play-dl');
-//const ytdl = require('ytdl-core');
-const { Player, Queue, Track } = require("discord-player");
-const { createAudioPlayer,
-    NoSubscriberBehavior,
-    joinVoiceChannel,
-    createAudioResource,
-    getVoiceConnection,
-    getGroups,
-    StreamType
-} = require('@discordjs/voice');
+const ytdl = require('ytdl-core');
+const { Player, Track } = require("discord-player");
+// const { createAudioPlayer,
+//     NoSubscriberBehavior,
+//     joinVoiceChannel,
+//     createAudioResource,
+//     getVoiceConnection,
+//     getGroups,
+//     StreamType
+// } = require('@discordjs/voice');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS,
         Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
@@ -20,6 +21,8 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS,
 });
 
 let connection = undefined;
+let queue = undefined;
+let radios = [];
 
 const player = new Player(client);
 
@@ -33,117 +36,191 @@ const BOT = {
             HELLO(userName) { return 'Hi ' + userName + ', glad to see you! 😄'},
         },
         OWNER: {
-            HELLO: 'Oh, yes, my overlord',
+            PLAY: 'Oh, yes, my overlord',
         },
         PLAYER: {
             START(title) { return `🎶 | Now playing **${title}**!`; },
+            STOP: 'Player stopped',
         },
         DREAM: 'Bye ☺️',
         BANANA: '💦',
-    },
-    PLAYLIST: {
-      OPEN: false,
-      LAST: '',
-
+        HELP: '',
 
     },
-
+    COMMANDS: ['play', 'skip', 'stop', 'back', 'to', 'remove', 'skip', 'list', 'help', 'dream'],
     GUILD: {
-        MEMBER: {
-
-        }
+        QUEUE: undefined,
+        LIST: undefined,
     },
+    //PLAYER: new Player(client),
+}
 
-    PLAYER: undefined,
-    QUEUE: undefined,
-    PLAYLISTS: [],
+function parseContent(content) {
+    let crumbs = content.trim().split(' ');
+    const botName = crumbs[0].toLowerCase();
+    let command = crumbs[1].toLowerCase();
+    let trackName = crumbs[2];
 
+    if(!BOT.COMMANDS.includes(command)) {
+        command = undefined;
+        trackName = crumbs.filter(crumb => {
+            return crumb !== BOT.NAME && !isUrl(crumb);
+        }).join(' ');
+    }
+    const url = crumbs.find(isUrl);
+
+    return { botName, command, trackName, url };
 }
 
 async function messageReact(message) {
     if (message.author.bot) return;
 
     const userName = message.author.username;
-    const messageCrumbs = message.content.trim().toLowerCase().split(' ');
+    const crumbs = parseContent(message.content);
 
-    switch (messageCrumbs[0]) {
+    console.log(crumbs);
+
+    switch (crumbs.botName) {
         case BOT.NAME:
-            await managePlayer(message, messageCrumbs.slice(1));
+            await managePlayer(message, crumbs);
             break;
         case '🍌':
             message.reply(BOT.PHRASE.BANANA);
             break;
 
         case 'hi,':
-            if(messageCrumbs[1] === BOT.NAME) message.reply(BOT.PHRASE.MEMBER.HELLO(userName));
+            if(message.content.includes(BOT.NAME)) message.reply(BOT.PHRASE.MEMBER.HELLO(userName));
             break;
-
-        // check messageCrumbs[1] in playlists/internet search
-
     }
 }
 
-async function managePlayer(message, [ firstCrumb, secondCrumb]) {
-    switch (firstCrumb) {
+async function managePlayer(message, { command, trackName, url }) {
+    switch (command) {
         case 'play':
             startPlayer(message);
 
-            if(message.author.username === BOT.OWNER) message.reply(BOT.PHRASE.OWNER.HELLO);
-            break;
+            //queue.play();
+            if(message.author.username === BOT.OWNER) message.reply(BOT.PHRASE.OWNER.PLAY);
+            return;
 
         case 'stop':
-            if(connection)  {
-                connection.disconnect();
-                connection = undefined;
-                message.reply('')
-            }
-            break;
+            queue.stop();
+            queue.jump(0);
+            // if(connection)  {
+            //     stopPlayer();
+            //     message.reply(BOT.PHRASE.PLAYER.STOP)
+            // }
+            return;
 
-        case 'pause':
-            break;
+        // case 'pause':
+        //    queue.stop();
+        //     return;
 
-        case 'next':
-            break;
+        case 'skip':
+            queue.skip();
+            return;
 
-        case 'prev':
-            break;
-
-        case 'open':
-            break;
+        case 'back':
+            queue.back();
+            return;
 
         case 'to':
-            break;
+            queue.jump(trackName);
+            return;
 
         case 'remove':
-            break;
+            queue.remove(trackName);
+            //radios.filter()
+            return;
+
+        case 'list':
+            // queue.tracks.forEach( track => console.log(track.title) );
+            // console.log(queue.tracks);
+            return;
 
         case 'help':
-            break;
+            return;
 
         case 'dream':
             if(message.author.username === BOT.OWNER) {
                 await message.reply(BOT.PHRASE.DREAM);
-
-                client.destroy();
-                process.exit();
-                return;
+                stopPlayer();
+                stopClient();
             }
+            return;
+        default:
             break;
+    }
 
+    if(url) {
+        const trackInfo = await ytdl.getInfo(url).then(info => info.videoDetails);
+        const track = new Track(player, { title: trackName ? trackName : trackInfo.title, url: url, source: 'youtube' });
+
+        if(trackInfo.isLiveContent) {
+            radios.push(track);
+            queue.play(track);
+        } else {
+            queue.addTrack(track);
+            queue.jump(track);
+        }
+
+    } else if (trackName) {
+        let isCurrentTrack = track => track.title === trackName;
+
+        let track = radios.find(isCurrentTrack);
+
+        if(track) queue.play(track);
+        else {
+            track = queue.tracks.find(isCurrentTrack);
+            if(track) queue.jump(track);
+            else {
+                track = await player.search(trackName, { requestedBy: message.author }).then(response => response.tracks[0]);
+                queue.addTrack(track);
+                queue.jump(track);
+            }
+        }
+    }
+
+
+}
+
+function isUrl(url) {
+    return url.includes('http') || url.includes('youtube');
+}
+
+function stopPlayer() {
+    if(connection){
+        connection.destroy();
+        connection = undefined;
     }
 }
 
+function stopClient() {
+    client.destroy();
+    process.exit();
+}
+
 async function startPlayer(message) {
-    const queue = player.createQueue(message.guild, {
+    queue = player.createQueue(message.guild, {
         metadata: {
             channel: message.channel
         }
     });
-    await  queue.connect(message.member.voice.channel);
-    const track = new Track(player, { title: 'lo-fi', url: 'https://www.youtube.com/watch?v=w3LWHIz3bMc', source: 'youtube'});
+    connection = await queue.connect(message.member.voice.channel);
+
+    const name = await ytdl.getInfo('https://www.youtube.com/watch?v=w3LWHIz3bMc').then(info => info.videoDetails.title);
+    const track = new Track(player, { title: name, url: 'https://www.youtube.com/watch?v=w3LWHIz3bMc', source: 'youtube'});
+
     await queue.play(track);
 }
 
+// player.on('queueEnd', queue => {
+//     queue.play();
+// });
+
+player.on("trackEnd", queue => {
+
+});
 
 player.on("trackStart", (queue, track) =>
     queue.metadata.channel.send(BOT.PHRASE.PLAYER.START(track.title))
@@ -165,8 +242,5 @@ client.once("ready", () => {
 
 client.login(token);
 
-// TODO: add link music support, add vk playlist music support,
-// TODO: create server playlists, add some radios, create music manager, fix connection destroy null exceptions
-
-// now //restart stream-play every source finish
-// TODO: fix stream ends
+// TODO: fix: connection destroy null exceptions, restart radio onerror, add: player controls, playlists, radios, find track, fix connect timeout error
+// TODO: fix multi word track names detecting, add normal crumber (not lower links and track names)
